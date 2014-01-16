9755b6c1cb31e51c25eca2cb7446fd2ed7bfc58d
vfa-taipa <taipa@vitalify.asia>
2014-01-16 14:08:21 +0700

    Update Client for get from STDIN

--- socket/Ruby/client.rb
+++ socket/Ruby/client.rb
@@ -1,10 +1,44 @@
-require 'socket'
+require "socket"
+require "thread"
 
-s = TCPSocket.new 'localhost', 2000
-s.write "hi server!"
+class ChatClient
+  def initialize(address, port)
+    @sockets = Array.new
 
-while line = s.gets # Read lines from socket
-  puts line         # and print them
+    @server = TCPSocket.new(address, port)
+    #puts("Ruby chat server started on port #{port}\n")
+    @sockets.push(@server)
+    @sockets.push($stdin)
+
+    run()
+  end
+
+  def run
+    while true
+
+      # The select method will take as an argument the array of sockets, and return a socket that has    
+      # data to be read
+      ioarray = select(@sockets, nil, nil, nil)
+
+        # the socket that returned data will be the first element in this array
+        for sock in ioarray[0]
+          if sock == $stdin then
+            @server.puts(sock.gets())
+          else
+            # Received something on a server socket
+            if sock.eof? then
+              str = "Client left #{sock.peeraddr[2]}"
+              sock.close
+              @sockets.delete(sock)
+            else
+              puts(sock.gets())
+            end
+          end
+        end
+    end
+  end
+
 end
 
-s.close             # close socket when done
+myChatServer = ChatClient.new("127.0.0.1", 2700)
+
