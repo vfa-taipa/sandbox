5c0ca11e0e610d28e2d36a57743794ac41b4dbbe
vfa-taipa <taipa@vitalify.asia>
2014-01-24 23:36:31 +0700

    Chomp

--- socket/Ruby/client.rb
+++ socket/Ruby/client.rb
@@ -33,7 +33,7 @@ class ChatClient
         # the socket that returned data will be the first element in this array
         for sock in ioarray[0]
           if sock == $stdin then
-            cmd = {"cmd" => 3, "roomid" => @roomid, "userid" => "", "content" => sock.gets()}
+            cmd = {"cmd" => 3, "roomid" => @roomid, "userid" => "", "content" => sock.gets().chomp}
             #@server.puts(sock.gets())
             @server.puts(JSON.generate(cmd))
           else
