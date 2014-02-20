fa293498c5f5de7ec0e11bdb940acb62dfcc5ddb
vfa-taipa <taipa@vitalify.asia>
2014-02-20 12:32:41 +0700

    aa

--- socket/UDP/client01.rb
+++ socket/UDP/client01.rb
@@ -1,5 +1,10 @@
 require 'socket'
 sock = UDPSocket.new
-data = 'I sent this'
-sock.send(data, 0, '127.0.0.1', 33333)
+# data = 'I sent this'
+# sock.send(data, 0, '127.0.0.1', 33333)
+(1..500).each do |i|
+	msg = "#{i}"
+	sock.send(msg, 0, '54.199.132.106', 8888)
+	sleep 0.02
+end
 sock.close
